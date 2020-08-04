924d4cf1ec82a4df2d2cf60e66041206c119f756
yamashin313 <araiba23@gmail.com>
2020-08-05 00:09:41 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<ul>
+			<li v-for="(row, index) in itemList" v-bind:key="index">
+				<label>{{ row.datetime }}：{{ row.content }}</label>
+				<button @click="deleteItem(index)">Delete</button>
+			</li>
+		</ul>
+		<div>
+			<span>Datetime:</span><input type="text" v-model="newDatetime">
+			<span>Content:</span><input type="text" v-model="newContent">
+			<button v-on:click="addItem()">Add Schedule</button>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		data(){
+			return{
+				itemList:[],
+				newDatetime: "",
+				newContent: ""
+			}
+		},
+		methods:{
+			addItem(){
+				if(this.newDatetime=="" || this.newContent==""){
+					alert("Please input value");
+					return;
+				}
+				this.itemList.push({
+					datetime: this.newDatetime,
+					content: this.newContent
+				});
+				this.newDatetime = "";
+				this.newContent = "";
+			},
+			deleteItem(index){
+				this.itemList.splice(index, 1)
+			}
+		}
+	}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
